ec5c686d0cb7bd1ee94c2e30923e2ec2e4f38ffe
ArduinoDenis <arduinodenis1@gmail.com>
2022-08-03 23:50:55 +0200

    update sito

--- js/script_google.js
+++ js/script_google.js
@@ -1,6 +1,4 @@
-<script type="text/javascript">
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments)}
-    gtag('js', new Date());
-    gtag('config', 'G-6VG0QVVXHH');
-</script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments)}
+gtag('js', new Date());
+gtag('config', 'G-6VG0QVVXHH');
