4f871dbf8d8343fc3d41a7811c944c2ea3475408
ArduinoDenis <arduinodenis1@gmail.com>
2022-08-03 23:40:43 +0200

    update index.html

--- js/script_google.js
+++ js/script_google.js
@@ -1,6 +1,6 @@
-<script>
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments)}
-gtag('js', new Date());
-gtag('config', 'G-6VG0QVVXHH');
+<script type="text/javascript">
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments)}
+    gtag('js', new Date());
+    gtag('config', 'G-6VG0QVVXHH');
 </script>
