bbc9ac2ff7145fcd553a137e295402d607778c16
arduinodenis <arduinodenis1@gmail.com>
2023-01-06 02:37:49 +0100

    I have updated the site and I have optimized the site

--- js/script_google.js
+++ js/script_google.js
@@ -1,4 +1,5 @@
 window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments)}
-gtag('js', new Date());
-gtag('config', 'G-6VG0QVVXHH');
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6VG0QVVXHH');
